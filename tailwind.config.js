ff090e90f9f932d23a78c39d29214f3a2a6614cf
Evgeniy Platnikov <127827144+Evgen31rus@users.noreply.github.com>
2024-06-11 22:20:34 +0300

    https://github.com/Evgen31rus/Portfolio-said.git

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,8 @@ module.exports = {
       'xl': {'min': '1280px', 'max': '1535px'},
       // => @media (min-width: 1280px and max-width: 1535px) { ... }
 
-      '2xl': {'min': '1536px'},
-      // => @media (min-width: 1536px) { ... }
+      '2xl': {'min': '1536px'}, 
+      // => @media (min-widt h: 1536px) { ... }
     },
     extend: {},
   },
