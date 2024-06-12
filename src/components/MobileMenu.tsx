28aff6dd098ee99bd1ce21f375ff4762cc3d935a
Evgeniy Platnikov <127827144+Evgen31rus@users.noreply.github.com>
2024-06-12 22:26:50 +0300

    all lags are fixed

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -7,7 +7,7 @@ export default function MobileMenu(){
     const isOpenMenu = useSelector((state:RootState)=>state.SliceMobileMenu.isOpen)
     const RussianLanguage = useSelector((state: RootState)=> state.switchLanguage.RussianLanguage)
     return(
-        <div className={` flex z-50 fixed w-[50vh] h-[100%] bg-[white] transition-all duration-300 justify-center items-center ${!isOpenMenu? 'translate-x-[-100%]':'translate-x-[40%]'} `}>
+        <div className={` flex z-50 fixed w-[45vh] h-[100%] bg-[white] transition-all duration-300 justify-center items-center ${!isOpenMenu? 'translate-x-[-100%]':'translate-x-[-10%]'} `}>
 
     <div className={`flex flex-col list-none w-[100%] h-[200px] justify-around items-center text-[black]  font-bmedium text-[2rem]`}>
           <a href={`#aboute`} className={`cursor-pointer`} onClick={()=>dispatch(HandleIsOpenMenu())}>{RussianLanguage? 'Обо мне' : 'About me'}</a>
